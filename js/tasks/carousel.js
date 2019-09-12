06c252d0cd5bea622c61ce1ff06bd9f4f7d3acc5
IrynaMukhina <imukhina@griddynamics.com>
2019-09-12 15:39:35 +0300

    upd

--- js/tasks/carousel.js
+++ js/tasks/carousel.js
@@ -1,48 +1,53 @@
 /* eslint-disable max-len */
-const slideIndex = [1, 1];
+const topNextBtn = document.getElementById('next_top');
+const bottomNextBtn = document.getElementById('next_bottom');
+const topPrevBtn = document.getElementById('prev_top');
+const bottomPrevBtn = document.getElementById('prev_bottom');
+
+const slideCounter = [1, 1];
 const slideId = ['container__speech_top', 'container__speech_bottom'];
 
 showSlides(1, 0);
 showSlides(1, 1);
 
 //  Next/previous controls
-function plusSlides(n, num) {
-  showSlides(slideIndex[num] += n, num);
+function changeSlide(slideNum, sliderIndex) {
+  showSlides(slideCounter[sliderIndex] += slideNum, sliderIndex);
 }
 
-function showSlides(n, num) {
+function showSlides(slideNum, sliderIndex) {
   let i;
-  const slide = document.getElementsByClassName(slideId[num]);
+  const slide = document.getElementsByClassName(slideId[sliderIndex]);
 
-  if (n > slide.length) {
-    slideIndex[num] = 1;
+  if (slideNum > slide.length) {
+    slideCounter[sliderIndex] = 1;
   }
 
-  if (n < 1) {
-    slideIndex[num] = slide.length;
+  if (slideNum < 1) {
+    slideCounter[sliderIndex] = slide.length;
   }
 
   for (i = 0; i < slide.length; i++) {
     slide[i].style.display = 'none';
   }
 
-  slide[slideIndex[num]-1].style.display = 'flex';
+  slide[slideCounter[sliderIndex]-1].style.display = 'flex';
 }
 
 // set event listener to buttons
 const carousel = ( function() {
   const init = () => {
-    document.getElementById('next_top').addEventListener('click', function() {
-      plusSlides(1, 0);
+    topNextBtn.addEventListener('click', function() {
+      changeSlide(1, 0);
     });
-    document.getElementById('next_bottom').addEventListener('click', function() {
-      plusSlides(1, 1);
+    bottomNextBtn.addEventListener('click', function() {
+      changeSlide(1, 1);
     });
-    document.getElementById('prev_top').addEventListener('click', function() {
-      plusSlides(-1, 0);
+    topPrevBtn.addEventListener('click', function() {
+      changeSlide(-1, 0);
     });
-    document.getElementById('prev_bottom').addEventListener('click', function() {
-      plusSlides(-1, 1);
+    bottomPrevBtn.addEventListener('click', function() {
+      changeSlide(-1, 1);
     });
   };
 
